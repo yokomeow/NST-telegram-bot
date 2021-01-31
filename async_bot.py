f66393d99a29cf0827084879c9178272399d48cc
yokomeow <doctorevrica@gmail.com>
2021-01-31 19:12:48 +0300

    some bug fixed

--- async_bot.py
+++ async_bot.py
@@ -10,16 +10,16 @@ from shutil import copyfile
 
 
 # webhook settings
-WEBHOOK_HOST = 'https://your.domain'
-WEBHOOK_PATH = '/path/to/api'
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+#WEBHOOK_HOST = 'https://your.domain'
+#WEBHOOK_PATH = '/path/to/api'
+#WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
-WEBAPP_HOST = 'localhost'  # or ip
-WEBAPP_PORT = 3001
+#WEBAPP_HOST = 'localhost'  # or ip
+#WEBAPP_PORT = 3001
 
 #port
-PORT = int(os.environ.get('PORT', 5000))
+#PORT = int(os.environ.get('PORT', 5000))
 
 # logging level
 logging.basicConfig(level=logging.INFO)
@@ -184,37 +184,37 @@ async def launch_nst(message):
     # await bot.send_message(message.chat.id, 'Чтобы попробовать еще раз, просто отправь мне новое фото. ☺️')
 
 
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
+#async def on_startup(dp):
+#    await bot.set_webhook(WEBHOOK_URL)
     # insert code here to run it after start
 
 
-async def on_shutdown(dp):
-    logging.warning('Shutting down..')
+#async def on_shutdown(dp):
+#    logging.warning('Shutting down..')
 
     # insert code here to run it before shutdown
 
     # Remove webhook (not acceptable in some cases)
-    await bot.delete_webhook()
+#    await bot.delete_webhook()
 
     # Close DB connection (if used)
-    await dp.storage.close()
-    await dp.storage.wait_closed()
+#    await dp.storage.close()
+#    await dp.storage.wait_closed()
 
-    logging.warning('Bye!')
+ #   logging.warning('Bye!')
 
 
 # launch long polling
 if __name__ == '__main__':
-    #executor.start_polling(dp, skip_updates=True)
-    start_webhook(listen='0.0.0.0', port=int(PORT), url_path=config.TOKEN)
-    executor.set_webhook('https://style-tg-bot.herokuapp.com/' + config.TOKEN)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+    executor.start_polling(dp, skip_updates=True)
+    #start_webhook(listen='0.0.0.0', port=int(PORT), url_path=config.TOKEN)
+    #executor.set_webhook('https://style-tg-bot.herokuapp.com/' + config.TOKEN)
+    #start_webhook(
+    #    dispatcher=dp,
+    #    webhook_path=WEBHOOK_PATH,
+    #    on_startup=on_startup,
+    #    on_shutdown=on_shutdown,
+    #    skip_updates=True,
+    #    host=WEBAPP_HOST,
+    #    port=WEBAPP_PORT,
+    #)
